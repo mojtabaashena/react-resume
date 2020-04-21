59337234fd5641aa55e0097c18082f145eaede00
Mojtaba <mojtaba.ashena@yahoo.com>
2020-04-22 00:32:47 +0430

    add main components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header'
+import About from './Components/About'
+import Menue from './Components/Menue';
+import Snowfall from 'react-snowfall'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="site" >
+        <Menue />
+        <Header />
+        <About />
+        <Snowfall
+  // Controls the number of snowflakes that are created (default 150)
+  snowflakeCount={20}
+/>
+        {/* <Resume data={this.state.resumeData.resume}/>
+        <Portfolio data={this.state.resumeData.portfolio}/>
+        <Testimonials data={this.state.resumeData.testimonials}/>
+        <Contact data={this.state.resumeData.main}/>
+        <Footer data={this.state.resumeData.main}/> */}
+      </div>
   );
 }
 
