bf2e0bbc3a1e921cd2a75f5c21080a37a466d004
Mojtaba <mojtaba.ashena@yahoo.com>
2020-05-01 11:53:37 +0430

    Updates

---
+++ precache-manifest.90bbd6e3e82097f7f94026b758beff34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c6f9efd8b192231c1eee0b4e720ec92",
+    "url": "/react-resume/index.html"
+  },
+  {
+    "revision": "dbfc7bb6b19c3b315469",
+    "url": "/react-resume/static/css/main.f600d928.chunk.css"
+  },
+  {
+    "revision": "1769764a31eab102ef27",
+    "url": "/react-resume/static/js/2.304b7291.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-resume/static/js/2.304b7291.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbfc7bb6b19c3b315469",
+    "url": "/react-resume/static/js/main.2295e487.chunk.js"
+  },
+  {
+    "revision": "f5c2930114e267054ceb",
+    "url": "/react-resume/static/js/runtime-main.2fa3ccf3.js"
+  }
+]);
